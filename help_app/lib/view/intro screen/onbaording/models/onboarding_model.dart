512625f58f0f729df2a6720b84b426a5de3496d6
patrick-codes <khofilyndon2018@gmail.com>
2023-12-31 21:15:04 +0000

    second commit

--- help_app/lib/view/intro screen/onbaording/models/onboarding_model.dart
+++ help_app/lib/view/intro screen/onbaording/models/onboarding_model.dart
@@ -18,7 +18,7 @@ class OnboardingContent {
 List<OnboardingContent> contentList = [
   OnboardingContent(
     image: 'assets/images/img1.png',
-    title: 'Who we are',
+    title: 'Who we are?',
     description:
         'This platform was built to make a difference in the lives of gender-based violence (GBV) survivors. Our team has a wealth of experience working with survivors, communities, and the criminal justice system.',
     backgroundColor: tertiaryColor,
